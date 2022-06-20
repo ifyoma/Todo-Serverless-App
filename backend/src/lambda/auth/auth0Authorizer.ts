ffbfed2fed5fb91967b7690261dd38e5e6d9ce5c
Silas Jimmy <jimmysilas17@gmail.com>
2022-06-20 11:20:30 +0300

    feat: Add basic code to deploy app

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,27 +1,32 @@
-import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
+import { CustomAuthorizerResult, CustomAuthorizerEvent } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify, decode } from 'jsonwebtoken'
+import {
+  // verify,
+  decode
+} from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
+// import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+// // TODO: Provide a URL that can be used to download a certificate that can be used
+// // to verify JWT token signature.
+// // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
+// const jwksUrl = '...'
 
 export const handler = async (
   event: CustomAuthorizerEvent
 ): Promise<CustomAuthorizerResult> => {
   logger.info('Authorizing a user', event.authorizationToken)
+
   try {
     const jwtToken = await verifyToken(event.authorizationToken)
     logger.info('User was authorized', jwtToken)
 
+    // Return IAM policy specifying the user's permissions
     return {
       principalId: jwtToken.sub,
       policyDocument: {
@@ -38,6 +43,7 @@ export const handler = async (
   } catch (e) {
     logger.error('User not authorized', { error: e.message })
 
+    // Deny the user access to resources
     return {
       principalId: 'user',
       policyDocument: {
@@ -57,6 +63,8 @@ export const handler = async (
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  console.log(jwt);
+
 
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
