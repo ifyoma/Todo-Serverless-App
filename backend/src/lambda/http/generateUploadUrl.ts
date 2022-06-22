c03613d7e48082b75ea7e450231e944466fdc6ca
Silas Jimmy <jimmysilas17@gmail.com>
2022-06-22 16:04:34 +0300

    feat: Add configuration to create s3 bucket and attach policy

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -7,9 +7,18 @@ import { cors, httpErrorHandler } from 'middy/middlewares'
 // import { createAttachmentPresignedUrl } from '../../businessLogic/todos'
 // import { getUserId } from '../utils'
 
+// const todosTable = process.env.TODOS_TABLE
+// const bucketName = process.env.ATTACHMENT_S3_BUCKET
+// const urlExpiration = process.env.SIGNED_URL_EXPIRATION
+
+// const s3 = new AWS.S3({
+//   signatureVersion: 'v4'
+// })
+
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     // const todoId = event.pathParameters.todoId
+    // const userId = getUserId(event)
 
     console.log("Processing event: ", event);
 
